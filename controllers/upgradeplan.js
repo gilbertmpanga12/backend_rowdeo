266e51c5e95dc8a28091b855a16d25e6b7adf591
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-12-07 23:01:29 +0300

    UPDATE: added upgrade plan

---
+++ controllers/upgradeplan.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const firestore = require('firebase-admin');
+const collection = 'rowdeousers';
+router.put('/', async function(req,res){
+    try{
+        const body = req.body.userId;
+        const user = await firestore.firestore()
+        .collection(collection).doc(body).update({subscriptionStatus: true});
+        res.status(204).send({message: "Successfully upgraded plan"});
+    }catch(error){
+        res.status(500).send({error});
+    }   
+});
+
+module.exports = router;
--- main.js
+++ main.js
@@ -4,16 +4,28 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require("helmet");
 const payments = require('./controllers/payments');
-const { initializeApp, applicationDefault } = require("firebase-admin/app");
+const upgradeplan = require('./controllers/upgradeplan');
+const admin = require("firebase-admin");
 const port = 3000;
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use('/create-payment-intent', payments);
-// initializeApp({
-//     credential: {},
-//     projectId: ""
-// });
+app.use('/upgrade-plan', payments);
+admin.initializeApp({
+    credential: admin.credential.cert({
+        "type": "service_account",
+        "project_id": "rowdeo-app",
+        "private_key_id": process.env.private_key_id,
+        "private_key": process.env.private_key,
+        "client_email": process.env.client_email,
+        "client_id": process.env.client_id,
+        "auth_uri": process.env.auth_uri,
+        "token_uri": process.env.token_uri,
+        "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+        "client_x509_cert_url": process.env.client_x509_cert_url
+    })
+});
 app.listen(port, () => {
 console.log("App running! ✅");
 });
